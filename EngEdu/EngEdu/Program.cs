4ffe35205eda0e737cb8df0ff71c0741f62160a9
tanakorn128 <60030041@kmitl.ac.th>
2018-08-24 16:00:13 +0700

    first year semester 1

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year2561 semster 1, T am  Enfedu feshman");
+        }
+    }
+}
