c9e32fe2bfc7a71a56f9a516fbfa441d499acb51
tanakorn128 <60030041@kmitl.ac.th>
2018-08-24 17:01:12 +0700

    2nd  year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -10,8 +10,9 @@ namespace EngEdu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year2561 semster 1, T am  Enfedu feshman");
-            Console.WriteLine("Year2561 semster 2, T am  I learn to Program in C sharp..");
+            Console.WriteLine("Year2561 semster 1, I am  Enfedu feshman.");
+            Console.WriteLine("Year2561 semster 2, I learn to Program in C sharp..");
+            Console.WriteLine("Year2560 semster 3, I start my program life. ");
 
         }
     }
