acff4533e02b07162952b08b192ff522f42cf0cc
tanakorn128 <60030041@kmitl.ac.th>
2018-08-24 16:06:01 +0700

    first year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -11,6 +11,8 @@ namespace EngEdu
         static void Main(string[] args)
         {
             Console.WriteLine("Year2561 semster 1, T am  Enfedu feshman");
+            Console.WriteLine("Year2561 semster 2, T am  I learn to Program in C sharp.");
+
         }
     }
 }
